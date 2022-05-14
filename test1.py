42fcfa00263a067d9cfd91deaa6a679e2800344b
Anton220488 <antonmtomsk@mail.ru>
2022-05-14 18:42:27 +0300

    version 1

--- test1.py
+++ test1.py
@@ -1,15 +1,15 @@
 
-import json
+# import json
 
 # from pizzahub import menu
 # f = open('test1.json', "w")
 # a = json.dump(menu, f)
 
-f = open('test1.json', "r")
-a = json.load(f)
-print(a)
-print(type(a))
-f.close()
+# f = open('test1.json', "r")
+# a = json.load(f)
+# print(a)
+# print(type(a))
+# f.close()
 
 
 
@@ -32,5 +32,54 @@ f.close()
 # f = open('text.txt', 'w+t')
 # f.write('New string')
 # f.close()
+
+
+
+# def factorial(n):
+#     if n == 0:
+#         return 1
+#
+#     return n * factorial(n+1)
+#
+# factorial(1)
+
+# def func():
+#     return True
+#
+# while func():
+#     print(1)
+
+
+
+
+
+# def factorial (n):
+#     if n == 0:
+#         return 1
+#     factorial(n - 1) * n
+#     return factorial(n - 1) * n
+#
+#
+# print (factorial(50))
+
+
+
+
+
+
+
+
+
+a = 6
+b = 4
+
+if a > b:
+    c = 6
+else:
+    c = 5
+
+# c = 6 if a > b else 5
+
+
 
 
